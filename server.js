1557d014ec711b829850cef345f4b562c135586a
Sumitpathak721 <pathaksumit721@gmail.com>
2023-10-01 19:31:11 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const _ = require('lodash');
+// const bodyParser = require("body-parser");
+
+// Route Decleartion
+const app = express();
+const PORT = 3000;
+
+// apis
+app.get("/api/blog-stats",async(req,res)=>{
+    try{
+        let blogs = await fetch("https://intent-kit-16.hasura.app/api/rest/blogs",{
+            method:"get",
+            headers:{
+                "x-hasura-admin-secret": "32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6"
+            }
+        });
+        blogs = (await blogs.json()).blogs;
+        const resData = {
+            totalBlogs:blogs.length,
+            totalPrivacyBlogs:_.size(_.filter(blogs, (blog) => {
+                return _.includes(blog.title.toLowerCase(), 'privacy')
+            })),
+            longestBlog:_.maxBy(blogs, blog =>  blog.title.length),
+            uniqueTitles:_.uniqBy(blogs, 'title').map(blog=>blog.title),
+        };
+        res.json({resData});
+    }catch(e){
+        console.log(e);
+        res.status(500).json({status:500,error:"Internal Server Error"})
+    }
+});
+
+app.get("/api/blog-search",async(req,res)=>{
+    try{
+        const {query} = req.query;
+        let blogs = await fetch("https://intent-kit-16.hasura.app/api/rest/blogs",{
+            method:"get",
+            headers:{
+                "x-hasura-admin-secret": "32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6"
+            }
+        });
+        blogs = (await blogs.json()).blogs;
+        const resData = _.filter(blogs, blog =>  _.includes(blog.title.toLowerCase(), query.toLowerCase()));
+        res.json({resData});
+    }catch(e){
+        res.status(500).json({status:500,error:"Internal Server Error"});
+    }
+});
+
+// blogs.filter(blog => blog.title.toLowerCase().includes('privacy')).length
+
+app.listen(PORT,()=>{
+    console.log("Listening to port",PORT)
+});
